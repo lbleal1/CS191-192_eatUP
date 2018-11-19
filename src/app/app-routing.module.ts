0bc3ed7e66d74e6fb315098ade9f01af01eba8de
Justine Paul Alvaro <jpalvaro@up.edu.ph>
2018-11-20 00:46:55 +0800

    Revert "ThemeFile-AppStructure-Routing"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { SignInComponent } from './auth/sign-in/sign-in.component';
-import { TrendingNewestComponent } from './home/trending-newest/trending-newest.component';
-const routes: Routes = [
-  { path: '', component: TrendingNewestComponent },
-  { path: 'sign-in', component: SignInComponent },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
